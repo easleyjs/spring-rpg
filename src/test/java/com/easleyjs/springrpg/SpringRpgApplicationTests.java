ca592e6e51d838a5c69e88553cd64f41ee4b8805
J.S. Easley <easleyjs@gmail.com>
2026-03-30 19:19:28 -0700

    Initial Spring setup

---
+++ src/test/java/com/easleyjs/springrpg/SpringRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easleyjs.springrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
