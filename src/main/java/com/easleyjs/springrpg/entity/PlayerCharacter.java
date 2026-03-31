5d573d4c76c4ad16c4aee0da83801f53a1c8081b
J.S. Easley <easleyjs@gmail.com>
2026-03-30 20:18:45 -0700

    Created Player entity starter code

---
+++ src/main/java/com/easleyjs/springrpg/entity/PlayerCharacter.java
@@ -0,0 +1,24 @@
+package com.easleyjs.springrpg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+public class PlayerCharacter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int xp;
+    private String weapon;
+}
