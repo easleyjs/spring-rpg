5ae84c81db1f95815ae81b188b3e446973e3fcba
J.S. Easley <easleyjs@gmail.com>
2026-04-03 11:18:51 -0700

    Created CombatService with 'attack' service.

--- src/main/java/com/easleyjs/springrpg/service/CombatService.java
+++ src/main/java/com/easleyjs/springrpg/service/CombatService.java
@@ -1,4 +1,27 @@
 package com.easleyjs.springrpg.service;
 
+import com.easleyjs.springrpg.dto.CombatResult;
+import com.easleyjs.springrpg.entity.PlayerCharacter;
+import org.springframework.stereotype.Service;
+import com.easleyjs.springrpg.repository.PlayerCharacterRepo;
+
+@Service
 public class CombatService {
+    private final PlayerCharacterRepo pcRepo;
+
+    public CombatService(PlayerCharacterRepo pcRepo) {
+        this.pcRepo = pcRepo;
+    }
+
+    public CombatResult attack(long pcId) {
+        PlayerCharacter pc = pcRepo.findById(pcId)
+                .orElseThrow(
+                        () -> new RuntimeException(
+                                String.format("Character with id %d not found", pcId)));
+        int damage = 10;
+        String message = String.format(
+                "%s attacks for %d damage.", pc.getName(), damage);
+
+        return new CombatResult(damage, message);
+    }
 }
