a776cfaa947e4184f3cf0ef3937ca9a545aaf245
J.S. Easley <easleyjs@gmail.com>
2026-04-03 12:19:13 -0700

    Created basic controller for combat

--- src/main/java/com/easleyjs/springrpg/controller/CombatController.java
+++ src/main/java/com/easleyjs/springrpg/controller/CombatController.java
@@ -1,4 +1,24 @@
 package com.easleyjs.springrpg.controller;
 
+import com.easleyjs.springrpg.dto.AttackRequest;
+import com.easleyjs.springrpg.dto.CombatResult;
+import com.easleyjs.springrpg.service.CombatService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/combat")
 public class CombatController {
+    private final CombatService service;
+
+    public CombatController(CombatService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/attack")
+    public CombatResult attack(@RequestBody AttackRequest request) {
+        return service.attack(request.getPlayerId());
+    }
 }
