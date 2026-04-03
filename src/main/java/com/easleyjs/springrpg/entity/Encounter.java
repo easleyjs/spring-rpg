c31de11ac9f6300e319037db8afcde1815125664
J.S. Easley <easleyjs@gmail.com>
2026-04-03 14:02:56 -0700

    Initial Encounter entity created.

---
+++ src/main/java/com/easleyjs/springrpg/entity/Encounter.java
@@ -0,0 +1,24 @@
+package com.easleyjs.springrpg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Encounter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long playerId;
+    private int playerHp;
+    private long monsterId;
+    private int monsterHp;
+
+}
