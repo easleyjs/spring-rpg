32308e6685a166baa91e6a17fea2d733d5425cad
J.S. Easley <easleyjs@gmail.com>
2026-04-07 18:09:01 -0700

    Created global exception handler

--- src/main/java/com/easleyjs/springrpg/exception/GlobalExceptionHandler.java
+++ src/main/java/com/easleyjs/springrpg/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.easleyjs.springrpg.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.easleyjs.springrpg.dto.ErrorResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidState(InvalidStateException e) {
+        return new ErrorResponse(e.getMessage());
+    }
 }
