0391ca072132f907953a9d722b05430955548081
J.S. Easley <easleyjs@gmail.com>
2026-04-07 17:39:07 -0700

    Created NotFoundException

---
+++ src/main/java/com/easleyjs/springrpg/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.easleyjs.springrpg.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
