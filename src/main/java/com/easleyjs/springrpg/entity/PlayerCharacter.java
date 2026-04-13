73bc64e614fab66b350d403bdb368092bed67d65
J.S. Easley <easleyjs@gmail.com>
2026-04-13 14:05:45 -0700

    Set up relationship between InventoryItem and Player

--- src/main/java/com/easleyjs/springrpg/entity/PlayerCharacter.java
+++ src/main/java/com/easleyjs/springrpg/entity/PlayerCharacter.java
@@ -1,12 +1,12 @@
 package com.easleyjs.springrpg.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -26,6 +26,8 @@ public class PlayerCharacter {
     private int weaponFlatBonus;
     private int weaponDmgMultiplier;
     private String armor;
+    @OneToMany(mappedBy = "player")
+    private List<InventoryItem> inventoryItems = new ArrayList<>();
 
     public PlayerCharacter(String name) {
         this.name = name;
