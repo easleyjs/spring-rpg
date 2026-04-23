9df6ba1d07269a5dbfa8b0f892fd12ddc3f23003
J.S. Easley <easleyjs@gmail.com>
2026-04-23 11:57:17 -0700

    Created User entity

---
+++ src/main/java/com/easleyjs/springrpg/entity/User.java
@@ -0,0 +1,29 @@
+package com.easleyjs.springrpg.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private PlayerCharacter player;
+
+    public User(
+            String username,
+            String password,
+            PlayerCharacter player
+    ) {
+        this.username = username;
+        this.password = password;
+        this.player = player;
+    }
+}
