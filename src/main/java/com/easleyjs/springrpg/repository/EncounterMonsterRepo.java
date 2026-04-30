14cf0329535fa355f0c7634fa21f892c5c4f56cd
J.S. Easley <easleyjs@gmail.com>
2026-04-29 19:17:52 -0700

    Renamed/fixed EncounterMonster items

---
+++ src/main/java/com/easleyjs/springrpg/repository/EncounterMonsterRepo.java
@@ -0,0 +1,7 @@
+package com.easleyjs.springrpg.repository;
+
+import com.easleyjs.springrpg.entity.EncounterMonster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EncounterMonsterRepo extends JpaRepository<EncounterMonster, Long> {
+}
