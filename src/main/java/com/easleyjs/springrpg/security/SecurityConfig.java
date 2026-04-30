d96e6c0d81910134f1917e75ab3aab8db87ef4fc
J.S. Easley <easleyjs@gmail.com>
2026-04-29 19:16:27 -0700

    Updated security config to allow html/js access

--- src/main/java/com/easleyjs/springrpg/security/SecurityConfig.java
+++ src/main/java/com/easleyjs/springrpg/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/**/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
