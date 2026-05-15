d61d40c2d804e175599617d6af21ef4c65de2b11
J.S. Easley <easleyjs@gmail.com>
2026-05-14 21:40:16 -0700

    Created service to specify role in User details

---
+++ src/main/java/com/easleyjs/springrpg/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.easleyjs.springrpg.service;
+
+import com.easleyjs.springrpg.repository.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    public CustomUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.easleyjs.springrpg.entity.User appUser = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(appUser.getUsername())
+                .password(appUser.getPassword())
+                .authorities("ROLE_" + appUser.getRole().name())
+                .build();
+    }
+}
