077feaa9c4a6e15fddb0b274644f8de573389ed9
J.S. Easley <easleyjs@gmail.com>
2026-06-11 20:46:48 -0700

    Refactored service and repo to use pc entity relation in Encounter

--- src/main/java/com/easleyjs/springrpg/repository/EncounterRepo.java
+++ src/main/java/com/easleyjs/springrpg/repository/EncounterRepo.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface EncounterRepo extends
         JpaRepository<Encounter, Long> {
-    Optional<Encounter> findByPlayerIdAndStatus(Long playerId, EncounterStatus status);
+    Optional<Encounter> findByPlayerCharacterIdAndStatus(Long id, EncounterStatus encounterStatus);
 }
